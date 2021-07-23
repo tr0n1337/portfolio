5e465ecd0faadd735bed372671e4c2b1e72e34a4
tr00n <thiago.souzax3@outlook.com>
2021-07-23 03:02:39 -0300

    :package: swap theme/scroll-reveal/mobile-menu/responsivity

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // Toggle menu when click in icon.
 const nav = document.querySelector("#header nav")
-const toggle = document.querySelectorAll("nav .toggle")
 const wm = document.querySelector(".show-wm")
 const up = document.querySelector("#btn-up")
+const toggle = document.querySelectorAll("nav .toggle")
 
 for (const i of toggle) {
   i.addEventListener('click', e => {
@@ -29,5 +29,37 @@ function swm(element) {
   arrayText.forEach((letter, i) => { setTimeout(e => {
   element.textContent += letter }, 100 * i)})
 }
-const nome = document.querySelector("#nome")
+const nome = document.querySelector(".nome")
 swm(nome)
+
+
+// SWAP THEME
+const theme = document.querySelector('html')
+const c = document.getElementById('checkbox')
+
+c.addEventListener('change', () => {
+  theme.classList.toggle('dark-theme')
+})
+
+// SCROLLREVEAL
+const scroll = ScrollReveal({
+  origin: 'top',
+  distance: '3rem',
+  duration: 700,
+  reset: true
+})
+
+scroll.reveal(`#home .container,
+               #about .title-about, #about .text-about, #about .circle-about,
+               #contact .title-contact, #contact .circle-contact, .sm-wrapper .git, .sm-wrapper .linkedin, .sm-wrapper .instagram`, {interval: 100} )
+
+
+// BACK TOP
+ const backtop = document.querySelector('.btn-up')
+ window.addEventListener('scroll', ()=>{
+   if(window.scrollY >= 1200){
+     backtop.classList.add('show-up')
+   }else{
+     backtop.classList.remove('show-up')
+   }
+ })
